f83f0ac42c5a707a2ea96b6f123bc2bf6100e67d
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-07-22 13:03:05 -0700

    Restructure frontend

--- app.js
+++ app.js
@@ -10,18 +10,20 @@ var config = require('./config');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, './public/views'));
-app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
@@ -39,7 +41,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    title: '404 | Ancien Eleves du College Vogt'
+  });
 });
 
 app.listen(config.port, function() {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ancien Eleves du College Vogt' });
+  res.render('index', {
+    title: 'Ancien Eleves du College Vogt'
+  });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    title: 'Login | Ancien Eleves du College Vogt'
+  });
 });
 
 module.exports = router;
