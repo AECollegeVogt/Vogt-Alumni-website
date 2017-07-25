6d443f366fd37abc6619495de5648b51839987ad
Diane27 <dianetobit@gmail.com>
2017-07-25 01:46:03 -0700

    add changes to login.ejs

---
+++ public/javascripts/main.js
@@ -0,0 +1,43 @@
+//datepicker for date of birth
+$(function(){
+	$( "#datepicker" ).datepicker({
+		dateFormat : 'mm/dd/yy',
+		changeMonth : true,
+		changeYear: true,
+		yearRange: '-100y:c+nn',
+		maxDate: '-1d'
+	});
+});
+
+//datepicker from-to
+$( function() {
+  var dateFormat = "mm/dd/yy",
+    from = $( "#from" )
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        changeYear: true
+      })
+      .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+      }),
+    to = $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      changeYear: true
+    })
+    .on( "change", function() {
+      from.datepicker( "option", "maxDate", getDate( this ) );
+    });
+
+  function getDate( element ) {
+    var date;
+    try {
+      date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+      date = null;
+    }
+
+    return date;
+  }
+} );
